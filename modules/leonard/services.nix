f8170ed7615a4c162a1eb3af59b58dc8045a8a15
LionsTech <lionstech@vivaldi.net>
2023-07-27 07:44:16 +0200

    Fix auto update: Quote flakes

--- modules/leonard/services.nix
+++ modules/leonard/services.nix
@@ -27,7 +27,7 @@
       # Keep in mind: This does not update flake inputs
       home-manager-auto-upgrade.Service.ExecStart = lib.mkForce (toString
         (pkgs.writeShellScript "home-manager-upgrade-script" ''
-          ${pkgs.home-manager}/bin/home-manager switch --flake github:lions-tech/personal-home-manager-config#leonard
+          ${pkgs.home-manager}/bin/home-manager switch --flake 'github:lions-tech/personal-home-manager-config#leonard'
         '')
       );
     };
--- modules/manuel/services.nix
+++ modules/manuel/services.nix
@@ -20,7 +20,7 @@
       # Keep in mind: This does not update flake inputs
       home-manager-auto-upgrade.Service.ExecStart = lib.mkForce (toString
         (pkgs.writeShellScript "home-manager-upgrade-script" ''
-          ${pkgs.home-manager}/bin/home-manager switch --flake github:lions-tech/personal-home-manager-config#manuel
+          ${pkgs.home-manager}/bin/home-manager switch --flake 'github:lions-tech/personal-home-manager-config#manuel'
         '')
       );
     };
