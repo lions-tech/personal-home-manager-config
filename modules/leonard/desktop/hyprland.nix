95b9f69dc1b61924b8557fd1974b34a1c31eb740
LionsTech <lionstech@vivaldi.net>
2023-12-19 11:53:15 +0100

    Hyprland: Instantly require password when locking

--- modules/leonard/desktop/hyprland.nix
+++ modules/leonard/desktop/hyprland.nix
@@ -14,7 +14,7 @@
           + " --indicator --indicator-radius 100 --indicator-thickness 7"
           + " --effect-blur 7x5 --effect-vignette 0.5:0.5"
           + " --ring-color 0ce9e9 --key-hl-color 067979 --line-color 00000000"
-          + " --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2");
+          + " --inside-color 00000088 --separator-color 00000000 --fade-in 0.2");
       in
       {
         "$mod" = "SUPER";
