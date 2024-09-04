19e9184af65e095d4276374cc3c134c7fbe5545f
LionsTech <lionstech@vivaldi.net>
2024-09-04 10:41:46 +0200

    Pin Zettlr config

--- modules/leonard/programs/zettlr/default.nix
+++ modules/leonard/programs/zettlr/default.nix
@@ -1,12 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let
+  configDir = ".config/Zettlr";
+in
 {
   home.packages = [ pkgs.zettlr ];
   home.file = {
     "custom.css" = {
       enable = true;
       source = ./custom.css;
-      target = ".config/Zettlr/custom.css";
+      target = "${configDir}/custom.css";
     };
+  };
+
+  home.activation = {
+    activateZettlr = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      run mkdir -p $HOME/${configDir}
+      run cp --no-preserve=all ${./config.json} ${configDir}/config.json
+    '';
   };
 }
